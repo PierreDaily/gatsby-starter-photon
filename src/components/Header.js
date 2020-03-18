731244f328d7d08105a2fdd6b0d64389684a2aa2
DESKTOP-T3GUSOS\pierr <contact@pierredaily.com>
2020-03-18 10:54:49 +0800

    storybook

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,30 @@
 import React from 'react'
+import './Header.scss'
 
 class Header extends React.Component {
-    render() {
-        return (
-            <section id="header">
-                <div className="inner">
-                    <span className="icon major fa-cloud"></span>
-                    <h1>With a vision “to make your business easier”</h1>
-                    <p>We are a company that provides one-stop HR Service.</p>
-                    <p>With us your business will be more cost-effectively and conveniently.</p>
-                    <ul className="actions">
-                        <li><a href="#one" className="button scrolly">Discover</a></li>
-                    </ul>
-                </div>
-            </section>
-        )
-    }
+  render() {
+    return (
+      <section id="header">
+        <div className="inner">
+          <span className="icon major fa-cloud"></span>
+          <h1>With a vision “to make your business easier”</h1>
+          <p>We are a company that provides one-stop HR Service.</p>
+          <p>
+            With us your business will be more cost-effectively and
+            conveniently.
+          </p>
+          <ul className="actions">
+            <li>
+              <a href="#one" className="button scrolly">
+                Discover
+              </a>
+            </li>
+          </ul>
+        </div>
+      </section>
+    )
+  }
 }
 
+const avc = e => 'dsfdfsfsf'
 export default Header
